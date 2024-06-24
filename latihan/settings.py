8b66880086a241909f9ed543e8754e1a4dd3fefa
syahbanipratama <syahbanipratama21@gmail.com>
2024-06-25 04:01:17 +0800

    update commit

--- latihan/settings.py
+++ latihan/settings.py
@@ -100,7 +100,11 @@ DATABASES = {
         'USER' : config('DB_USER'),
         'PASSWORD' : config('DB_PASSWORD'),
         'HOST' : config('DB_HOST'),
-        'PORT' : config('DB_PORT')
+        'PORT' : config('DB_PORT'),
+        'OPTIONS': {
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset' : 'utf8mb4',
+        }
     }
 }
 
